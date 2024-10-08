114b1613652edef6b3ba4b220ac6a201e220ede9
eungyo <eungyo0965@gmail.com>
2024-10-08 04:42:36 +0200

    add Sidebar

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,32 +1,45 @@
 import {React,useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 import { Container, Flex,Text, Button, Box, Image, VStack,Input } from '@chakra-ui/react'
 
 
 const AuthForm = () => {
-    const [isLogin, setIsLogin]=useState(true)
+    const [isLogin, setIsLogin]=useState(true);
+    const navigate=useNavigate();
+    const [inputs,setInputs]=useState({
+        email:'',
+        password:'',
+        confirmPassword:''
+    })
 
+    const handleAuth=() =>{
+        if(inputs.email || inputs.password ){
+            alert("Please fill all the fields");
+            return;
+        }
+        navigate("/");
+    }
   return <>
   <Box border={"1px solid gray"} borderRadius={4} padding={5}>
     <VStack spacing={4}>
         <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
-        <Input 
-        placeholder='Email'
-        fontSize={14}
-        type='email'
+        <Input placeholder='Email' fontSize={14} type='email'
+        value={inputs.email}
+        onChange={(e)=>setInputs({...inputs,email:e.target.value})}
          />
-        <Input 
-        placeholder='Password'
-        fontSize={14}
-        type='password'
+        <Input placeholder='Password' fontSize={14} type='password'
+         value={inputs.password}
+         onChange={(e)=>setInputs({...inputs,password:e.target.value})}
          />
 
-         {!isLogin ? <Input 
-        placeholder='Confirm Password'
+         {!isLogin ? <Input placeholder='Confirm Password'
+         value={inputs.cinfirmPassword}
+         onChange={(e) => setInputs({...inputs,confirmPassword:e.target.value})}
         fontSize={14}
         type='password'
          /> : null}
 
-         <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} >
+         <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} onClick={handleAuth}>
             {isLogin? "Login" : "Sign Up"}
          </Button>
 
