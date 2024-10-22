35b8e154a16bec85e68131d3d72d827b385f54ed
eungyo <eungyo0965@gmail.com>
2024-10-22 06:02:49 +0200

    refactor(createPost) : remove part that inserts post info into browser local storage -It is better not to store massive datas in localstorage

--- src/hooks/useCreatePost.js
+++ src/hooks/useCreatePost.js
@@ -9,34 +9,43 @@ import usePostStore from "../store/postStore";
 
 const useCreatePost = () => {
 	const [isUpdating, setIsUpdating] = useState(false);
-	const showToast = useShowToast();
-	const uploadImage=useUploadImage();
+
+	const uploadImage=useUploadImage(); //firestore에 업로드한 이미지 url가져옴
+
+    //zustand에서 전역상태 불러옴
     const authUser=useAuthStore((state)=>state.user);
     const createPost=usePostStore(state => state.createPost)
     // const addPost=useUserProfileStore(state=>state.addPost);
 
-    const fuckPost = async (caption,selectedFile) => {
-        if(!selectedFile) throw new Error('Please select an image');
+    const showToast = useShowToast();
+
+    const createNewPost = async (caption,selectedFile) => {
+        if(isUpdating)return;
 		setIsUpdating(true);
-        const newPost = {
-            caption: caption,
-            likes:[],
-            comments: [],
-            createdAt: new Date().now(),
-            createdBy: authUser.uid,
-        };
+
+        let URL="";//이미지 파일 url
 
 		try {
-            //add post img to storage
+            if(selectedFile){
+                URL=await uploadImage(selectedFile, "/postPic"); 
+            }
+
+            const newPost = {
+                caption: caption,
+                likes:[],
+                comments: [],
+                createdAt: new Date().now(),
+                createdBy: authUser.uid,
+                imageUrl: URL,
+            };
+
+            //firestore에 post 컬렉션 추가
             const postDocRef=await addDoc(collection(firestore,"posts"),newPost);
             const userDocRef=doc(firestore,"users",authUser.uid);
-            const downloadURL=uploadImage("posts");
 
-            await updateDoc(userDocRef,{posts:arrayUnion(postDocRef.id)})
-            
-            await updateDoc(postDocRef,{imageURL:downloadURL});
-            
-            newPost.imgURL=downloadURL;
+            await updateDoc(userDocRef,{posts:arrayUnion(postDocRef.id)}) //authUser객체의 posts필드에 포스트 ID추가
+
+            //zustand 전역상태관리에 새포스트 추가
             createPost({...newPost,id:postDocRef.id});
 
             showToast("Success", "Post created successfully", "success");
@@ -47,7 +56,7 @@ const useCreatePost = () => {
         }
 	};
 
-	return { fuckPost, isUpdating };
+	return { createNewPost, isUpdating };
 };
 
 export default useCreatePost;
--- src/hooks/useEditProfile.js
+++ src/hooks/useEditProfile.js
@@ -8,17 +8,23 @@ import useUserProfileStore from "../store/userProfileStore";
 import useUploadImage from "./useUploadImg";
 
 const useEditProfile = () => {
-	const [isUpdating, setIsUpdating] = useState(false);
-	const authUser = useAuthStore((state) => state.user);
+	const [isUpdating, setIsUpdating] = useState(false); //로딩중 상태관리
+
+	//authStore전역 상태관리
+	const authUser = useAuthStore((state) => state.user); 
 	const setAuthUser = useAuthStore((state) => state.setUser);
 	const setUserProfile = useUserProfileStore((state) => state.setUserProfile);
+
 	const showToast = useShowToast();
+
+	//firebase storage에 이미지 업로드 -> 이미지 url 반환
 	const uploadImage=useUploadImage();
 
 	const editProfile = async (inputs,selectedFile) => {
 		if (isUpdating || !authUser) return;
 		setIsUpdating(true);
 
+		//firestore에 docs url 반환
 		const userDocRef = doc(firestore, "users", authUser.uid);
 		let URL = "";
 
@@ -38,9 +44,13 @@ const useEditProfile = () => {
 			// Firestore에 업데이트 수행
 			await updateDoc(userDocRef, updatedUser);
 
+			//브라우저 로컬 저장소에 user-info업데이트
 			localStorage.setItem("user-info", JSON.stringify(updatedUser));
+
+			//zustnad 상태저장소에 업데이트
 			setAuthUser(updatedUser);
 			setUserProfile(updatedUser);
+			
 			showToast("Success", "Profile updated successfully", "success");
 		} catch (error) {
 			showToast("Error", error.message, "error");
